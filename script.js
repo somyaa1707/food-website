39a9e0d8e0a98039808b378512c950ce3d952d70
somyaa1707 <somyapal07@gmail.com>
2025-06-21 14:49:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navigation
+document.querySelectorAll('.nav-links a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
